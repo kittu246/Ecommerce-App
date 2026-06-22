c09410e9daf98189bd24c3efc57c6d5c1b6f9443
kritika <kritikashyup18@gmail.com>
2026-06-22 16:16:31 +0530

    collectio filter added

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,10 +1,68 @@
-import React, { useContext,useState } from 'react'
+import React, { useContext,useState,useEffect } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets'
+import Title from '../components/Title';
+import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
-  const {products}=useContext(ShopContext)
-  const [showFilter,setShowFilter]= useState(false);
+  const {products} = useContext(ShopContext)
+  const [showFilter,setShowFilter] = useState(false);
+  const [filterProducts,setFilterProducts] = useState([]);
+  const [category,setCategory] = useState([]);
+  const [subCategory,setSubCategory] = useState([]);
+
+
+  const toggleCategory=(e)=>{
+    if(category.includes(e.target.value)){
+      setCategory( prev=>prev.filter((item)=> item !== e.target.value))
+    }
+    else{
+      setCategory(prev=>[...prev,e.target.value])
+    }
+
+  }
+
+   const toggleSubCategory=(e)=>{
+    if(subCategory.includes(e.target.value)){
+      setSubCategory( prev =>prev.filter((item)=> item !== e.target.value))
+    }
+    else{
+      setSubCategory(prev =>[...prev,e.target.value])
+    }
+
+  }
+
+  const applyFilter=()=>{
+    let copyProducts = products.slice();
+
+    if(category.length>0){
+    copyProducts = copyProducts.filter(item => category.includes(item.category))
+   
+
+    }
+
+    if(subCategory.length >0){
+    copyProducts = copyProducts.filter(item => subCategory.includes(item.subCategory))
+  
+
+    }
+     
+   
+  setFilterProducts(copyProducts);
+  // when ther is no category and subcategory then setFilterProducts(products), 
+  // so no need to separately write useEffect for initial showing all products.  
+
+
+
+  }
+
+ useEffect(()=>{
+applyFilter();
+
+},[category,subCategory])
+
+
+
 
   const handleMobileFilters=()=>{
     setShowFilter(!showFilter);
@@ -21,13 +79,13 @@ const Collection = () => {
           <p className="text-sm font-medium mb-3">CATEGORIES</p>
           <div className='flex flex-col gap-2 text-gray-700 text-sm font-light'>
             <p className='flex gap-2 '>
-            <input className='w-3' type="checkbox" value={"Men"} />Men
+            <input className='w-3' type="checkbox" value={"Men"} onChange={toggleCategory} />Men
           </p>
           <p className='flex gap-2 '>
-            <input className='w-3' type="checkbox" value={"Women"} />Women
+            <input className='w-3' type="checkbox" value={"Women"} onChange={toggleCategory} />Women
           </p>
           <p className='flex gap-2 '>
-            <input className='w-3' type="checkbox" value={"Child"} />Child
+            <input className='w-3' type="checkbox" value={"Child"} onChange={toggleCategory} />Child
           </p>
           </div>
           
@@ -38,17 +96,41 @@ const Collection = () => {
           <p className="text-sm font-medium mb-3">TYPE</p>
           <div className='flex flex-col gap-2 text-gray-700 text-sm font-light'>
             <p className='flex gap-2 '>
-            <input className='w-3' type="checkbox" value={"Topwear"} />Topwear
+            <input className='w-3' type="checkbox" value={"Topwear"} onChange={toggleSubCategory} />Topwear
           </p>
           <p className='flex gap-2 '>
-            <input className='w-3' type="checkbox" value={"Bottomwear"} />Bottomwear
+            <input className='w-3' type="checkbox" value={"Bottomwear"} onChange={toggleSubCategory} />Bottomwear
           </p>
           <p className='flex gap-2 '>
-            <input className='w-3' type="checkbox" value={"Winterwear"} />Winterwear
+            <input className='w-3' type="checkbox" value={"Winterwear"} onChange={toggleSubCategory} />Winterwear
           </p>
           </div>
           
 
+        </div>
+
+      </div>
+
+      <div className='flex-1'>
+        <div className='flex justify-between text-base sm:text-2xl mb-4'>
+          <Title text1={'ALL'} text2={'COLLECTIONS'} />
+          <select className='border-2 border-gray-300 text-sm px-2'>
+
+            <option value="relevant">Sort By: Relevant</option>
+            <option value="low-high">Sort By: Low to High</option>
+            <option value="high-low">Sort By: High to Low</option>
+
+          </select>
+        </div>
+        {/* filter products options */}
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-6'>
+
+          {filterProducts.map((item,index)=>(
+            <ProductItem id={index} name={item.name} image={item.image} price={item.price} />
+          ))}
+
+
+             
         </div>
 
       </div>
